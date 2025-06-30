7f3fb7d0f591dc78b84e3b87f5aed136be2174ea
Dom Urso <duom22@gmail.com>
2025-06-30 10:42:39 -0400

    adding addition and subtraction for start

---
+++ app/operations.py
@@ -0,0 +1,26 @@
+class Operation:
+
+    @staticmethod
+    def addition(a: float,b:float) -> float:
+        
+        """
+        Adds two values of type float
+        **Params**
+            - *a (float)*: First float value
+            - *b (float)*: Second float value
+        **Returns**
+            - *float*: Sum of *a* and *b*
+        """
+        return a+b
+    
+    @staticmethod
+    def subtraction(a:float,b:float) -> float:
+        """
+        Subracts two values of type float
+        **Params**
+            - *a (float)*: First float value
+            - *b (float)*: Second float value
+        **Returns**
+            - *float*: difference of *a* and *b*
+        """
+        return a-b
